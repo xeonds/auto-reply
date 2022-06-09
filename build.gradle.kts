2a05ff53d3b9015d2c1d5e7e29d344744b38058a
xeonds <1456506329@qq.com>
2022-06-09 21:25:29 +0800

    配置工程

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("net.mamoe.mirai-console") version "2.10.0"
 }
 
-group = "org.example"
+group = "team.gugugu"
 version = "0.1.0"
 
 repositories {
--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,4 +1,4 @@
-package org.example.mirai.plugin
+package xyz.xeonds.mirai.autoreply
 
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
@@ -14,80 +14,33 @@ import net.mamoe.mirai.message.data.Image.Key.queryUrl
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.utils.info
 
-/**
- * 使用 kotlin 版请把
- * `src/main/resources/META-INF.services/net.mamoe.mirai.console.plugin.jvm.JvmPlugin`
- * 文件内容改成 `org.example.mirai.plugin.PluginMain` 也就是当前主类全类名
- *
- * 使用 kotlin 可以把 java 源集删除不会对项目有影响
- *
- * 在 `settings.gradle.kts` 里改构建的插件名称、依赖库和插件版本
- *
- * 在该示例下的 [JvmPluginDescription] 修改插件名称，id和版本，etc
- *
- * 可以使用 `src/test/kotlin/RunMirai.kt` 在 ide 里直接调试，
- * 不用复制到 mirai-console-loader 或其他启动器中调试
- */
-
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
-        id = "org.example.mirai-example",
-        name = "插件示例",
+        id = "xyz.xeonds.mirai.autoreply",
+        name = "自动回复",
         version = "0.1.0"
     ) {
-        author("作者名称或联系方式")
+        author("xeonds@stu.xidian.edu.cn")
         info(
             """
-            这是一个测试插件, 
-            在这里描述插件的功能和用法等.
+            根据关键词自动触发回复。详情见Readme文档
         """.trimIndent()
         )
-        // author 和 info 可以删除.
     }
 ) {
     override fun onEnable() {
-        logger.info { "Plugin loaded" }
+        logger.info { "Auto-reply loaded" }
         //配置文件目录 "${dataFolder.absolutePath}/"
         val eventChannel = GlobalEventChannel.parentScope(this)
         eventChannel.subscribeAlways<GroupMessageEvent>{
-            //群消息
-            //复读示例
-            if (message.contentToString().startsWith("复读")) {
-                group.sendMessage(message.contentToString().replace("复读", ""))
-            }
-            if (message.contentToString() == "hi") {
-                //群内发送
-                group.sendMessage("hi")
-                //向发送者私聊发送消息
-                sender.sendMessage("hi")
-                //不继续处理
-                return@subscribeAlways
-            }
-            //分类示例
-            message.forEach {
-                //循环每个元素在消息里
-                if (it is Image) {
-                    //如果消息这一部分是图片
-                    val url = it.queryUrl()
-                    group.sendMessage("图片，下载地址$url")
-                }
-                if (it is PlainText) {
-                    //如果消息这一部分是纯文本
-                    group.sendMessage("纯文本，内容:${it.content}")
-                }
-            }
+
         }
         eventChannel.subscribeAlways<FriendMessageEvent>{
-            //好友信息
-            sender.sendMessage("hi")
+
         }
-        eventChannel.subscribeAlways<NewFriendRequestEvent>{
-            //自动同意好友申请
-            accept()
-        }
-        eventChannel.subscribeAlways<BotInvitedJoinGroupRequestEvent>{
-            //自动同意加群申请
-            accept()
-        }
+    }
+
+    override fun onDisable() {
+        logger.info{"Auto-reply unloaded."}
     }
 }
